3aa7ca802d09bf08b22ac914140609510f6a5c2c
wrb94 <209150@student.pwr.edu.pl>
2018-09-01 12:36:55 +0200

    [#2.3]

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Switch, TextInput, AsyncStorage } from 'react-native';
+import { StyleSheet, View, ScrollView, Switch, TextInput, AsyncStorage, TouchableWithoutFeedback } from 'react-native';
 import { Constants } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
 
 const DONE = 'Done';
 const NOT_DONE = 'NotDone';
@@ -9,8 +10,12 @@ const NOTES_STORAGE_KEY = 'notes';
 export default class App extends React.Component {
     state = {
         notes: [],
-        filter: DONE,
-        newNoteText: ''
+        filter: NOT_DONE,
+        newNoteText: '',
+        editElement: {
+            newNoteText: '',
+            index: 0
+        }
     };
 
     async componentDidMount() {
@@ -44,6 +49,42 @@ export default class App extends React.Component {
         AsyncStorage.setItem(NOTES_STORAGE_KEY, JSON.stringify(this.state.notes));
     };
 
+    onElementEdit = (newNoteText, index) => {
+        this.setState({
+            editElement: {
+                newNoteText,
+                index
+            }
+        });
+    };
+
+    onElementSubmit = () => {
+        this.setState(prevState => {
+            const notes = prevState.notes;
+            notes[prevState.editElement.index].message = prevState.editElement.newNoteText;
+            notes[prevState.editElement.index].creationDate = new Date().toLocaleTimeString();
+            AsyncStorage.setItem(NOTES_STORAGE_KEY, JSON.stringify(notes));
+
+            return {
+                notes,
+                editElement: {
+                    newNoteText: '',
+                    index: 0
+                }
+            };
+        });
+    };
+
+    removeElement = index => {
+        this.setState(prevState => {
+            const notes = prevState.notes;
+            notes.splice(index, 1);
+            AsyncStorage.setItem(NOTES_STORAGE_KEY, JSON.stringify(notes));
+
+            return { notes };
+        });
+    };
+
     render() {
         return (
             <View style={styles.container}>
@@ -52,16 +93,22 @@ export default class App extends React.Component {
                     placeholder="Add note"
                     onSubmitEditing={this.onInputSubmit}
                     onChangeText={this.onInputChange}
+                    autoFocus
                 />
                 <Switch value={this.state.filter === DONE} onValueChange={this.onSwitchStatus} />
                 <ScrollView>
-                    <View>
-                        {this.state.notes.filter(note => note.isDone === this.state.filter).map((note, index) => (
-                            <Text key={index} style={note.isDone === DONE ? styles.doneTask : styles.notDoneTask}>
-                                {`${index + 1}. ${note.message}  ${note.creationDate}`}
-                            </Text>
-                        ))}
-                    </View>
+                    {this.state.notes.filter(note => note.isDone === this.state.filter).map((note, index) => (
+                        <View key={index}>
+                            <TextInput
+                                value={note.message}
+                                onChangeText={value => this.onElementEdit(value, index)}
+                                onSubmitEditing={this.onElementSubmit}
+                            />
+                            <TouchableWithoutFeedback onPress={() => this.removeElement(index)}>
+                                <Ionicons name="md-close" size={12} color="red" />
+                            </TouchableWithoutFeedback>
+                        </View>
+                    ))}
                 </ScrollView>
             </View>
         );
@@ -75,11 +122,5 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center'
-    },
-    doneTask: {
-        color: 'green'
-    },
-    notDoneTask: {
-        color: 'red'
     }
 });
