af30c2c9e5a635eee8349a8210acfc9a1ae6b1d0
wrb94 <209150@student.pwr.edu.pl>
2018-09-01 11:09:44 +0200

    [#1.4]

--- App.js
+++ App.js
@@ -1,33 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Switch } from 'react-native';
 import { Constants } from 'expo';
+
+const DONE = 'Done';
+const NOT_DONE = 'NotDone';
 
 export default class App extends React.Component {
     state = {
-        notes: new Array(100).fill(0).map((_, index) => `Note ${index}`)
+        notes: new Array(100).fill(0).map((_, index) => ({
+            message: `Note ${index + 1}`,
+            isDone: Math.random() > 0.5 ? DONE : NOT_DONE
+        })),
+        filter: DONE
+    };
+
+    onSwitchStatus = () => {
+        this.setState(prevState => ({
+            filter: prevState.filter === DONE ? NOT_DONE : DONE
+        }));
     };
 
     render() {
         return (
-            <ScrollView style={styles.scrollView}>
-                <View style={styles.container}>
-                    {this.state.notes.map((note, index) => (
-                        <Text key={index}>{note}</Text>
-                    ))}
-                </View>
-            </ScrollView>
+            <View style={styles.container}>
+                <Switch value={this.state.filter === DONE} onValueChange={this.onSwitchStatus} />
+                <ScrollView>
+                    <View>
+                        {this.state.notes.filter(note => note.isDone === this.state.filter).map((note, index) => (
+                            <Text key={index} style={note.isDone === DONE ? styles.doneTask : styles.notDoneTask}>
+                                {`${index + 1}. ${note.message}`}
+                            </Text>
+                        ))}
+                    </View>
+                </ScrollView>
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
     container: {
+        paddingTop: Constants.statusBarHeight,
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center'
     },
-    scrollView: {
-        paddingTop: Constants.statusBarHeight
+    doneTask: {
+        color: 'green'
+    },
+    notDoneTask: {
+        color: 'red'
     }
 });
